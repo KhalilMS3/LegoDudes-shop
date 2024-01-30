5662e3042ada7eae6877ce5af638484e35d9d0bf
KhalilMS3 <kmsfouk@hiof.no>
2024-01-30 12:39:38 +0100

    Header.jsx
    page header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,32 @@
+export default function Header (){
+  return (
+    <header>
+            <img id="logo" src="website_images/LD_logo.svg" alt="LEGOdudes nettbutikk" />
+            <button id="carttoggle">
+                <span id="cartcount">99</span>
+                <img id="carticon" src="website_images/legocart.svg" alt="Din handlevogn" />
+            </button>
+            <section id="cart">
+                <h2>Din handlevogn</h2>
+                <ul id="cartlist">
+                    <li>
+                        <span className="title">Produkttittel</span>
+                        <span className="price">89,-</span>
+                        <span className="quantity">x1</span>
+                        <span className="functions">
+                            <button>X</button>
+                        </span>
+                    </li>
+                    <li>
+                        <span className="title">Wengestone warriors</span>
+                        <span className="price">9989,-</span>
+                        <span className="quantity">x333</span>
+                        <span className="functions">
+                            <button>X</button>
+                        </span>
+                    </li>
+                </ul>
+            </section>
+        </header>
+  )
+}
