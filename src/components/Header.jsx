5d3e75ad9d8c5e77b338aa38272ea7360cdf8e66
KhalilMS3 <kmsfouk@hiof.no>
2024-01-30 12:56:46 +0100

    Update Header.jsx
    cart section replaced with Cart compnent

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import Cart from "./Cart"
 export default function Header (){
   return (
     <header>
@@ -6,27 +7,7 @@ export default function Header (){
                 <span id="cartcount">99</span>
                 <img id="carticon" src="website_images/legocart.svg" alt="Din handlevogn" />
             </button>
-            <section id="cart">
-                <h2>Din handlevogn</h2>
-                <ul id="cartlist">
-                    <li>
-                        <span className="title">Produkttittel</span>
-                        <span className="price">89,-</span>
-                        <span className="quantity">x1</span>
-                        <span className="functions">
-                            <button>X</button>
-                        </span>
-                    </li>
-                    <li>
-                        <span className="title">Wengestone warriors</span>
-                        <span className="price">9989,-</span>
-                        <span className="quantity">x333</span>
-                        <span className="functions">
-                            <button>X</button>
-                        </span>
-                    </li>
-                </ul>
-            </section>
+            <Cart/>
         </header>
   )
 }
